05090d7be490b10a7ed85e70697cfb92c02fc458
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2023-11-19 21:38:27 +0200

    feature: check_private_functions

---
+++ private_functions/contract-rs/src/lib.rs
@@ -0,0 +1,34 @@
+// Find all our documentation at https://docs.near.org
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::env::log_str;
+use near_sdk::near_bindgen;
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+    answer: u8,
+}
+
+impl Default for Contract {
+    fn default() -> Self {
+        Self { answer: 7 }
+    }
+}
+
+#[near_bindgen]
+impl Contract {
+    // Get the answer (public)
+    pub fn get_answer(&self) -> u8 {
+        log_str(format!("The answer is {}", self.answer).as_str());
+        self.answer
+    }
+
+    // 1. Write a internal function "add" that's only callable by the contracts code
+    // The function should accept two integers (u8) a and b and returns the sum of the two parameters
+
+    // 2. Write a public function "get_sum" that accepts two integers (u8) a and b and returns the sum of the two parameters
+    // Make use of the internal function "add" that you wrote above
+
+    // 3. Write a public function "set_answer" that's only callable (private) from the Account ID that the contract has been deployed to
+    // The function should set the answer to 42 (self.answer = 42)
+}
