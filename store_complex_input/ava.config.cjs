d2c00f14177b845600a0700fd7bfc5fa7c711644
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2023-11-28 16:33:49 +0200

    rm complex_input

--- store_complex_input/ava.config.cjs
+++
@@ -1,9 +0,0 @@
-require("util").inspect.defaultOptions.depth = 5; // Increase AVA's printing depth
-
-module.exports = {
-  timeout: "10000",
-  files: ["ava-tests/*.ava.ts"],
-  failWithoutAssertions: false,
-  extensions: ["ts"],
-  require: ["ts-node/register"],
-};
