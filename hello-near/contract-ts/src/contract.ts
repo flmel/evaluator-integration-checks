2ef99d2f4a64b876111f758fa3bc451db7b38201
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2023-12-12 11:48:53 +0200

    feat: add hello-near evaluation

---
+++ hello-near/contract-ts/src/contract.ts
@@ -0,0 +1,9 @@
+import { NearBindgen } from 'near-sdk-js';
+
+@NearBindgen({})
+class Contract {
+  greeting: string = "Hello";
+  // TODO: Add a public function `get_greeting` that returns the current greeting from contracts state
+
+  // TODO: Add a public function `set_greeting` that takes a string argument `greeting` and sets the contracts state to that value
+}
